b0b35eb6496d4e8086ba66e6340f6b187551a51e
Dducws868 <daominhduc010129@gmail.com>
2026-09-01 11:05:13 +0700

    Create templateShort.cpp

---
+++ template/templateShort.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+#define mp make_pair
+#define F first
+#define S second
+
+#define tcT template <class T
+#define tcTU tcT, class U
+tcT > using V = vector<T>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+#define sz(x) int(size(x))
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define F0R(i, a) FOR(i, 0, a - 1)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define rep(i, a) F0R(i, a)
+#define each(a, x) for (auto &a : x)
+
+tcT > bool ckmin(T &a, const T &b) {
+    return b < a ? a = b, 1 : 0;
+}
+tcT > bool ckmax(T &a, const T &b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+const int MOD = 998244353;  // 1e9+7;
+const int MAX = (int)1e6 + 100;
+const int LOG = 20;
+const ll INF = 1e18;
+const db PI = acos((db)-1);
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+
+    return 0;
+}
