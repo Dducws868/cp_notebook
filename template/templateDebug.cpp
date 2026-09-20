648eae0ef0d8412dd852d978455c63af4a57db1b
Dducws868 <daominhduc010129@gmail.com>
2026-09-20 23:22:58 +0700

    Update templateDebug.cpp

--- template/templateDebug.cpp
+++ template/templateDebug.cpp
@@ -1,12 +1,12 @@
 #ifndef DEBUG_TEMPLATE_CPP
 #define DEBUG_TEMPLATE_CPP
 #include <bits/stdc++.h>
-// #define cerr cout
+
 namespace __DEBUG_UTIL__
 {
     using namespace std;
+
     /* Primitive Datatypes Print */
-    // Xử lý riêng cho các kiểu cơ bản
     void print(int x) { cerr << x; }
     void print(long x) { cerr << x; }
     void print(long long x) { cerr << x; }
@@ -20,61 +20,77 @@ namespace __DEBUG_UTIL__
     void print(const char *x) { cerr << '\"' << x << '\"'; }
     void print(const string &x) { cerr << '\"' << x << '\"'; }
     void print(bool x) { cerr << (x ? "true" : "false"); }
+
     template <size_t N>
     void print(bitset<N> x) { cerr << x; }
+
     void print(vector<bool> v)
-    { /* Overloaded this because stl optimizes vector<bool> by using
-          _Bit_reference instead of bool to conserve space. */
+    {
         int f = 0;
         cerr << '{';
         for (auto &&i : v)
             cerr << (f++ ? "," : "") << (i ? "T" : "F");
         cerr << "}";
     }
-    /* Templates Declarations to support nested datatypes */
+
+    /* Template Declarations to support nested datatypes */
     template <typename T>
-    void print(T &&x);
-    template <typename T>
-    void print(vector<vector<T>> mat);
+    void print(const vector<vector<T>> &mat);
+
     template <typename T, size_t N, size_t M>
     void print(T (&mat)[N][M]);
+
     template <typename F, typename S>
-    void print(pair<F, S> x);
+    void print(const pair<F, S> &x);
+
     template <typename T, size_t N>
     struct Tuple;
+
     template <typename T>
     struct Tuple<T, 1>;
+
     template <typename... Args>
-    void print(tuple<Args...> t);
+    void print(const tuple<Args...> &t);
+
     template <typename... T>
     void print(priority_queue<T...> pq);
+
     template <typename T>
     void print(stack<T> st);
+
     template <typename T>
     void print(queue<T> q);
+
+    template <typename T>
+    void print(const T &x);
+
     /* Template Datatypes Definitions */
     template <typename T>
-    void print(T &&x)
+    void print(const T &x)
     {
-        /*  This works for every container that supports range-based loop
-            i.e. vector, set, map, oset, omap, dequeue */
         int f = 0;
         cerr << '{';
-        for (auto &i : x)
-            cerr << (f++ ? "," : ""), print(i);
+        for (auto &i : x) {
+            cerr << (f++ ? "," : "");
+            print(i);
+        }
         cerr << "}";
     }
+
     template <typename T>
-    void print(vector<vector<T>> mat)
+    void print(const vector<vector<T>> &mat)
     {
         int f = 0;
         cerr << "\n~~~~~\n";
         for (auto &&i : mat)
         {
-            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+            cerr << setw(2) << left << f++;
+            print(i);
+            cerr << "\n";
         }
         cerr << "~~~~~\n";
     }
+
     template <typename T, size_t N, size_t M>
     void print(T (&mat)[N][M])
     {
@@ -82,12 +98,15 @@ namespace __DEBUG_UTIL__
         cerr << "\n~~~~~\n";
         for (auto &&i : mat)
         {
-            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+            cerr << setw(2) << left << f++;
+            print(i);
+            cerr << "\n";
         }
         cerr << "~~~~~\n";
     }
+
     template <typename F, typename S>
-    void print(pair<F, S> x)
+    void print(const pair<F, S> &x)
     {
         cerr << '(';
         print(x.first);
@@ -95,60 +114,77 @@ namespace __DEBUG_UTIL__
         print(x.second);
         cerr << ')';
     }
+
     template <typename T, size_t N>
     struct Tuple
     {
-        static void printTuple(T t)
+        static void printTuple(const T &t)
         {
             Tuple<T, N - 1>::printTuple(t);
-            cerr << ",", print(get<N - 1>(t));
+            cerr << ",";
+            print(get<N - 1>(t));
         }
     };
+
     template <typename T>
     struct Tuple<T, 1>
     {
-        static void printTuple(T t) { print(get<0>(t)); }
+        static void printTuple(const T &t) { print(get<0>(t)); }
     };
+
     template <typename... Args>
-    void print(tuple<Args...> t)
+    void print(const tuple<Args...> &t)
     {
         cerr << "(";
         Tuple<decltype(t), sizeof...(Args)>::printTuple(t);
         cerr << ")";
     }
+
     template <typename... T>
     void print(priority_queue<T...> pq)
     {
         int f = 0;
         cerr << '{';
-        while (!pq.empty())
-            cerr << (f++ ? "," : ""), print(pq.top()), pq.pop();
+        while (!pq.empty()) {
+            cerr << (f++ ? "," : "");
+            print(pq.top());
+            pq.pop();
+        }
         cerr << "}";
     }
+
     template <typename T>
     void print(stack<T> st)
     {
         int f = 0;
         cerr << '{';
-        while (!st.empty())
-            cerr << (f++ ? "," : ""), print(st.top()), st.pop();
+        while (!st.empty()) {
+            cerr << (f++ ? "," : "");
+            print(st.top());
+            st.pop();
+        }
         cerr << "}";
     }
+
     template <typename T>
     void print(queue<T> q)
     {
         int f = 0;
         cerr << '{';
-        while (!q.empty())
-            cerr << (f++ ? "," : ""), print(q.front()), q.pop();
+        while (!q.empty()) {
+            cerr << (f++ ? "," : "");
+            print(q.front());
+            q.pop();
+        }
         cerr << "}";
     }
+
     /* Printer functions */
-    void printer(const char *) {} /* Base Recursive */
+    inline void printer(const char *) {} /* Base Recursive */
+
     template <typename T, typename... V>
     void printer(const char *names, T &&head, V &&...tail)
     {
-        /* Using && to capture both lvalues and rvalues */
         int i = 0;
         for (size_t bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket != 0); i++)
             if (names[i] == '(' or names[i] == '<' or names[i] == '{')
@@ -157,13 +193,17 @@ namespace __DEBUG_UTIL__
                 bracket--;
         cerr.write(names, i) << " = ";
         print(head);
-        if (sizeof...(tail))
-            cerr << " ||", printer(names + i + 1, tail...);
-        else
+        if (sizeof...(tail)) {
+            cerr << " || ";
+            printer(names + i + 1, tail...);
+        } else {
             cerr << "]\n";
+        }
     }
+
     /* PrinterArr */
-    void printerArr(const char *) {} /* Base Recursive */
+    inline void printerArr(const char *) {} /* Base Recursive */
+
     template <typename T, typename... V>
     void printerArr(const char *names, T arr[], size_t N, V... tail)
     {
@@ -173,21 +213,36 @@ namespace __DEBUG_UTIL__
         for (ind++; names[ind] and names[ind] != ','; ind++)
             ;
         cerr << " = {";
-        for (size_t i = 0; i < N; i++)
-            cerr << (i ? "," : ""), print(arr[i]);
+        for (size_t i = 0; i < N; i++) {
+            cerr << (i ? "," : "");
+            print(arr[i]);
+        }
         cerr << "}";
-        if (sizeof...(tail))
-            cerr << " ||", printerArr(names + ind + 1, tail...);
-        else
+        if (sizeof...(tail)) {
+            cerr << " || ";
+            printerArr(names + ind + 1, tail...);
+        } else {
             cerr << "]\n";
+        }
+    }
+
+    inline void err_prefix(const char *func, int line, const char *args) {
+        std::cerr << "\033[0;31m\u001b[1mDEBUG\033[0m"
+                  << " | "
+                  << "\u001b[34m" << func << "\033[0m"
+                  << ":"
+                  << "\u001b[34m" << line << "\033[0m"
+                  << " - "
+                  << "[" << args << "] = [";
     }
 }
+
 #ifndef ONLINE_JUDGE
-#define debug(...) std::cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
-#define debugArr(...) std::cerr << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#define debug(...) __DEBUG_UTIL__::err_prefix(__FUNCTION__, __LINE__, #__VA_ARGS__), __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define debugArr(...) __DEBUG_UTIL__::err_prefix(__FUNCTION__, __LINE__, #__VA_ARGS__), __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
 #else
 #define debug(...)
 #define debugArr(...)
 #endif
-#endif
 
+#endif
