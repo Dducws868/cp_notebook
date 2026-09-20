d4b09b545651ed80b1810fc5de2ab419d2e02df6
Dducws868 <daominhduc010129@gmail.com>
2026-09-20 21:42:22 +0700

    Create templateShort.cpp

---
+++ template/templateShort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using str = string;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+#define mp make_pair
+#define F first
+#define S second
+
+#define tcT template <class T
+#define tcTU tcT, class U
+tcT > using V = vector<T>;
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+
+tcT > bool ckmin(T &a, const T &b) {
+    return b < a ? a = b, 1 : 0;
+}
+tcT > bool ckmax(T &a, const T &b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+const int MAX = (int)1e6 + 100;
+const ll INF = 1e18;
+
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+
+    return 0;
+}
